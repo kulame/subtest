c04e50aa23f55058d4ddae44758b2e4561919cff
kula <kula@live.com>
2022-07-06 08:21:20 +0800

    feat: 🎸 use polkadot

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import '@polkadot/api-augment/polkadot'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
--- src/pok/pok.ts
+++ src/pok/pok.ts
@@ -1,8 +1,9 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
 export async function connect() {
-    const wsProvider = new WsProvider("wss://192.168.31.221");
+    const wsProvider = new WsProvider("wss://rpc.polkadot.io");
     const api = await ApiPromise.create({provider:wsProvider})
+
     const unsub = await api.query.system.account('',acc =>console.log);
     console.log("------------")
     console.log(unsub)
